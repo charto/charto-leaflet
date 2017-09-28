615ebc1f28f1de60ff28436d4e4898c5b86099eb
jjrv <juha@busfaster.com>
2017-09-28 13:53:18 +0300

    Add margin to tiles when fetching data.

--- src/CanvasLayer.ts
+++ src/CanvasLayer.ts
@@ -15,21 +15,39 @@ export class CanvasLayer extends L.GridLayer {
 
 	createTile(xyz: PointZ, cb: (err: any, canvas: HTMLCanvasElement) => void) {
 		const canvas = L.DomUtil.create('canvas', 'leaflet-tile leaflet-tile-loaded') as HTMLCanvasElement;
-
-		const bounds: L.LatLngBounds = (this as any)._tileCoordsToBounds(xyz);
 		const size = this.getTileSize();
 
 		canvas.width = size.x;
 		canvas.height = size.y;
 
-		const sw = bounds.getSouthWest();
-		const ne = bounds.getNorthEast();
-		const bbox = new BBox(sw.lat, sw.lng, ne.lat, ne.lng);
+		const map: L.Map = (this as any)._map;
+		const margin = 8;
 
-		new Promise((resolve: (layerFeatures: LayerFeaturesPromise) => void) =>
+		const x = xyz.x * size.x;
+		const y = xyz.y * size.y;
+		const zoom = xyz.z;
+
+		new Promise((resolve: (layerFeatures: LayerFeaturesPromise) => void) => {
+			// Get bounding box in world coordinates for the tile,
+			// surrounded by a margin measured in pixels to handle symbols
+			// overlapping this tile but centered within neighbor tile edges.
+			const sw = map.unproject(new L.Point(x - margin, y + size.y + margin), zoom);
+			const ne = map.unproject(new L.Point(x + size.x + margin, y - margin), zoom);
+			const bbox = new BBox(sw.lat, sw.lng, ne.lat, ne.lng);
+
+			// Fetch features inside the bounding box from the model layer.
 			resolve(this.layer.getLayerFeatures(bbox, size.x, size.y))
-		).then((features: LayerFeatures) =>
-			this.renderer.render(canvas, features, bbox, size.x, size.y, xyz.z)
+		}).then((features: LayerFeatures) => {
+			// Get tile bounding box in world coordinates.
+			const sw = map.unproject(new L.Point(x, y + size.y), zoom);
+			const ne = map.unproject(new L.Point(x + size.x, y), zoom);
+			const bbox = new BBox(sw.lat, sw.lng, ne.lat, ne.lng);
+
+			return(this.renderer.render(canvas, features, bbox, size.x, size.y, zoom));
+		}).catch(
+			// Ignore errors in tile rendering for now.
+			// TODO: Maybe draw a warning watermark?
+			() => {}
 		).then(() =>
 			cb(null, canvas)
 		);
